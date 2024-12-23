23d07de8841d892bd638f932e969a399f44de53e
Svetlin Mladenov <svetlinm@pm.me>
2024-12-23 13:40:37 +0200

    Tokenizing ==

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -148,6 +148,11 @@ from xyc.tokenizer import split_tokens
         ['a', '+=', 'b', ';'],
         None
     ),
+    (
+        'a == b; a <= b; a |= b;',
+        ['a', '==', 'b', ';', 'a', '<=', 'b', ';', 'a', '|=', 'b', ';'],
+        None
+    ),
 ])
 def test_split_tokens(code, tokens, token_pos):
     res = split_tokens(code)
