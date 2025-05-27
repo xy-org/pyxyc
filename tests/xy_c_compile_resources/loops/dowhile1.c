8d1499bcaf80d0e54a6a9897eb3e1bfc3a98ba97
Svetlin Mladenov <svetlinm@pm.me>
2025-05-27 14:56:26 +0300

    Reorder do/while tests

--- tests/xy_c_compile_resources/loops/dowhile1.c
+++ tests/xy_c_compile_resources/loops/dowhile1.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-int32_t dowhiles_step(void);
+int32_t dowhile1_step(void);
 
-int32_t dowhiles_simpleDoWhile(int32_t p_x, int32_t p_y) {
+int32_t dowhile1_simpleDoWhile(int32_t p_x, int32_t p_y) {
     int32_t l_i = 0;
     do {
         p_x += 10;
@@ -13,14 +13,14 @@ int32_t dowhiles_simpleDoWhile(int32_t p_x, int32_t p_y) {
     return l_i;
 }
 
-int32_t dowhiles_write(int32_t p_lim) {
+int32_t dowhile1_write(int32_t p_lim) {
     int32_t tmp_0_total = 0;
     do {
-        tmp_0_total += dowhiles_step();
+        tmp_0_total += dowhile1_step();
     } while (tmp_0_total < p_lim);
     return tmp_0_total;
 }
 
-int32_t dowhiles_step(void) {
+int32_t dowhile1_step(void) {
     return 10;
 }
