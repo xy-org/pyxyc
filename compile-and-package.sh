8e770316b4fdf09e908981269fb150283c7e05a2
Svetlin Mladenov <svetlinm@pm.me>
2025-05-29 16:21:12 +0300

    Don't use AppImage to package xyc
    Just build an executable using pyinstaller and zip it together with all of its deps

--- compile-and-package.sh
+++ compile-and-package.sh
@@ -9,26 +9,29 @@ set -e
 python cython-setup.py build_ext --inplace
 
 # package with pyinstaller into a directory (no --nofile) which ensure speedy bootstrap
+# NOTE packaging with --onefile produces significantly slower binary
 pyinstaller \
     --hiddenimport glob --hiddenimport difflib \
     --name="xyc" --add-binary="xyc/*.so:xyc/" --add-data="xyc/libs:xyc/libs" \
     xyc/xyc.py
 
-# and then for the "coup de grace" package again using AppImage in order to have a single executable file
-ln -s xyc dist/xyc/AppRun
-cat <<EOF > dist/xyc/xyc.desktop
-[Desktop Entry]
-Name=xyc
-Exec=xyc
-Icon=icon-xyc
-Type=Application
-Categories=Utility;Application;
-EOF
-wget "https://www.xy-lang.org/images/logo.svg" -O "dist/xyc/icon-xyc.svg"
+## and then for the "coup de grace" package again using AppImage in order to have a single executable file
+#ln -s xyc dist/xyc/AppRun
+#cat <<EOF > dist/xyc/xyc.desktop
+#[Desktop Entry]
+#Name=xyc
+#Exec=xyc
+#Icon=icon-xyc
+#Type=Application
+#Categories=Utility;Application;
+#EOF
+#wget "https://www.xy-lang.org/images/logo.svg" -O "dist/xyc/icon-xyc.svg"
+#
+#wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O build/linuxdeploy-x86_64.AppImage
+#chmod u+x build/linuxdeploy-x86_64.AppImage
+#./build/linuxdeploy-x86_64.AppImage --appdir dist/xyc --output appimage
 
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -O build/linuxdeploy-x86_64.AppImage
-chmod u+x build/linuxdeploy-x86_64.AppImage
-./build/linuxdeploy-x86_64.AppImage --appdir dist/xyc --output appimage
+#mkdir -p bin
+#mv xyc-x86_64.AppImage bin/xyc-x86_64
 
-mkdir -p bin
-mv xyc-x86_64.AppImage bin/xyc-x86_64
+cd dist/xyc && tar -cvzf ../../xyc-$(uname | awk '{print tolower($0)}').tar.gz *
