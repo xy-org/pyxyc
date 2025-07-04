6da49a8c9f4fbb28187bbcef654bd82182aed9ab
Svetlin Mladenov <svetlinm@pm.me>
2025-07-04 15:16:42 +0300

    Tests for srcof and srclineof

--- tests/xy_c_compile_resources/ctti/srcLocFuncs.c
+++ tests/xy_c_compile_resources/ctti/srcLocFuncs.c
@@ -16,4 +16,7 @@ void srcLocFuncs_test(void) {
     const uint32_t l_f = 14;
     int8_t* const l_g = (int8_t*)"tests/xy_c_compile_resources/ctti/srcLocFuncs.xy";
     const uint32_t l_h = 16;
+    int8_t* const l_i = (int8_t*)"1 * 10 + 20";
+    int8_t* const l_j = (int8_t*)"    j := (1 * 10 + 20)'srclineof;";
+    int8_t* const l_k = (int8_t*)"    k := 'srclineof;";
 }
